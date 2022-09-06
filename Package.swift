5d8f42a387d5f4a829c34dd76bd0ad3d05902abd
Rodrigo Dutra de Oliveira <rodrigo@voltpost.com>
2022-09-05 23:34:18 -0300

    SPM platforms configuration

--- Package.swift
+++ Package.swift
@@ -6,8 +6,10 @@ import PackageDescription
 let package = Package(
     name: "CoreMLHelpers",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12)
+        .iOS(.v11),
+        .macOS(.v12),
+        .tvOS(.v11),
+        .watchOS(.v6)
     ],
     products: [
         .library(
